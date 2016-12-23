40abec64097a0c1c3b0114cd4d9adb5da6cffda8
Heinz N. Gies <heinz@licenser.net>
2016-12-23 01:44:17 +0100

    fix make rel dep

--- manifests/svcs.pp
+++ manifests/svcs.pp
@@ -8,7 +8,7 @@ class fifo_test::svcs ( $svc ){
   service { "svc:/network/${svc}:default":
     require => [
                 Service[ 'svc:/pkgsrc/epmd:default' ],
-                Exec[ "make_rel_${svcs}" ],
+                Exec[ "make_rel_${svc}" ],
                 File[ "/opt/local/fifo-${svc}/share" ],
                 File[ "/opt/local/fifo-${svc}/bin" ],
                 File[ "/opt/local/fifo-${svc}/lib" ],
